6e1297dab918a883261a39e0f4f817d6642f2887
Torben Masur <torben.masur@haw-hamburg.de>
2017-11-07 13:42:05 +0100

    Neue Klasse für die Ansteuerung der Motoren

---
+++ Motor.cpp
@@ -0,0 +1,18 @@
+/*
+ * Motor.cpp
+ *
+ *  Created on: 07.11.2017
+ *      Author: torbe
+ */
+
+#include "Motor.h"
+
+Motor::Motor() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Motor::~Motor() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Motor.h
@@ -0,0 +1,17 @@
+/*
+ * Motor.h
+ *
+ *  Created on: 07.11.2017
+ *      Author: torbe
+ */
+
+#ifndef MOTOR_H_
+#define MOTOR_H_
+
+class Motor {
+public:
+	Motor();
+	virtual ~Motor();
+};
+
+#endif /* MOTOR_H_ */
