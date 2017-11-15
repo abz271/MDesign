fb25420d2ae2c21e867b0c19045c679702eb64f0
Torben Masur <torben.masur@haw-hamburg.de>
2017-11-15 20:30:43 +0100

    Motorsteuerung komplett neu geschrieben, und einfach getestet

--- Motor.cpp
+++ Motor.cpp
@@ -1,156 +1,210 @@
 #include "Motor.h"
-#include <math.h>
-#include <stdlib.h>
+#include <Arduino.h>
 
-Motor::Motor(){
-
-
-}
-
-Motor::Motor(unsigned char pin, unsigned char dirpin) {
-    /* Konstruktor berechnet zum Anfang des Programms die Lookup-Table, um spätere Berechnungen zu beschleunigen.
-     * Pins für Geschwindigkeit und Richtung werden ebenfalls hier gesetzt
-     */
-
-    // Pin-Zuweisung
-    this->pin = pin;
-    this->dirpin = dirpin;
-
-    // Erstellung der Beschleunigungs-Lookup-Table
-    int ramp_size;
-    int max_a_size;
-    int adj_formfactor;
-
-    float conversion_factor;
-    float ramp_incline;
-
-    this->samplerate = SAMPLERATE;
-    this->formfactor = FORMFACTOR;
-    this->a_max = AMAX;
-
-    // Berechnung der Beschleunigungsrampe: Umrechnungen
-    conversion_factor = (float) samplerate / 100;
-    adj_formfactor = (int) (conversion_factor * formfactor);
-    ramp_size = samplerate - adj_formfactor;
-    max_a_size = adj_formfactor - ramp_size;
-
-    // Berechnung der Beschleunigungsrampe: Diskretisierung
-    ramp_incline = (float) a_max / (float) ramp_size;
-    for (int i = 0; i < ramp_size; i++) {
-        a[i] = (int) round(i * ramp_incline);
-    }
-
-    // Plateau maximaler Beschleunigung
-    for (int i = ramp_size - 1; i < SAMPLERATE / 2; i++) {
-        a[i] = a_max;
-    }
-
-    // Initialisiere Werte
-    v = 0;
-    index = 0;
+Motor::Motor() {
 }
 
 Motor::~Motor() {
-    // nothing to see here~~~
 }
 
-void Motor::calcMovement(unsigned char v1) {
-    /* Vorbereitung der Beschleunigungsberechnung. Ermittlung der PWM-Differenz.
-     * Wird bei jeder neuen Bewegung ausgeführt (jedoch nicht bei kleinen Kurskorrekturen).
-     */
-    this->v1 = v1;
-    v0 = v;
-    v_delta = abs(this->v1 - v0);
-    v_delta_half = v_delta / 2;
-    v1greaterv0 = this->v1 > v0 ? true : false;
-    index = 0;
-    ramp_started = false;
-    ramp_symmetry = false;
-    compensation = false;
+void Motor::updateVelocity() {
+
+	// Zeitliches Verhalten, nur alle 0,05s den nächsten Schritt machen
+	static int startTimer = millis();
+	static int stopTimer = millis();
+
+	if (millis() >= startTimer + startStepTime) {
+
+		startTimer = millis();
+		// Links
+		if (currentVelocityPwmLeft < nextVelocityPwmLeft) {
+
+			// Beschleunigen
+			currentVelocityPwmLeft += startStepPwm;
+
+			/*	Wenn die Geschwindigkeit die maximale Geschwindigkeit übersteigt
+			 * 	wird diese auf das Maximun gesetzt. Dies wird zusätzlich benötigt, damit
+			 * 	das System nicht wieder langsamer wird
+			 */
+			if (currentVelocityPwmLeft >= nextVelocityPwmLeft) {
+				currentVelocityPwmLeft = nextVelocityPwmLeft;
+			}
+
+		}
+		// Rechts
+		if (currentVelocityPwmRight < nextVelocityPwmRight) {
+
+			// Beschleunigen
+			currentVelocityPwmRight += startStepPwm;
+
+			/*	Wenn die Geschwindigkeit die maximale Geschwindigkeit übersteigt
+			 * 	wird diese auf das Maximun gesetzt. Dies wird zusätzlich benötigt, damit
+			 * 	das System nicht wieder langsamer wird
+			 */
+			if (currentVelocityPwmRight >= nextVelocityPwmRight) {
+				currentVelocityPwmRight = nextVelocityPwmRight;
+			}
+
+		}
+
+		// Geschwindigkeiten updaten
+		analogWrite(pwmA, currentVelocityPwmLeft);
+		analogWrite(pwmB, currentVelocityPwmRight);
+	}
+
+	if (millis() >= stopTimer + stopStepTime) {
+
+		stopTimer = millis();
+		// Links
+		if (currentVelocityPwmLeft > nextVelocityPwmLeft) {
+
+			// Abbremsen
+			currentVelocityPwmLeft -= stoptStepPwm;
+
+			/*	Wenn die Geschwindigkeit kleiner als die minimale Geschwindigkeit 0 ist,
+			 * 	wird diese auf das Minimum gesetzt
+			 */
+			if (currentVelocityPwmLeft <= 0) {
+				currentVelocityPwmLeft = 0;
+			}
+
+		}
+
+		// Rechts
+		if (currentVelocityPwmRight > nextVelocityPwmRight) {
+
+			// Abbremsen
+			currentVelocityPwmRight -= stoptStepPwm;
+
+			/*	Wenn die Geschwindigkeit kleiner als die minimale Geschwindigkeit 0 ist,
+			 * 	wird diese auf das Minimum gesetzt
+			 */
+			if (currentVelocityPwmRight <= 0) {
+				currentVelocityPwmRight = 0;
+			}
+
+		}
+
+		// Geschwindigkeiten updaten
+		analogWrite(pwmA, currentVelocityPwmLeft);
+		analogWrite(pwmB, currentVelocityPwmRight);
+	}
 }
 
-unsigned char Motor::calcPWM(bool emergencyOverride) {
-    /* Berechnung der Beschleunigung im Taktzyklus des Kurskorrektur-Algorithmus.
-     * emergencyOverride schaltet die Beschleunigungsberechnung aus und gibt direkt den gewünschten PWM-Wert aus.
-     * Falls der Unterschied zwischen altem und neuen PWM-Wert < JMAX, beschleunigt calcPWM direkt auf neuen PWM-Wert
-     * Gibt neuen PWM-Wert zurück.
-     */
-    unsigned char lut_sigma = a_max / 2 + 1;
-    unsigned char v_delta_to_half = abs(abs(v1 - v) - v_delta_half);
-    //printf("v_current_delta: %u\n", v_delta_to_half);
+void Motor::driveStraight() {
 
-    if (!emergencyOverride) {
-        if (abs(v1 - v0) < JMAX) {
-            // Springt zu neuem PWM-Wert, wenn Unterschied klein ist
-            index = 0;
-            v = v1;
-        } else if (v < 255) {
-            // Beschleunigungsberechnung anhand Lookup-Table
-            v = v1greaterv0 ? v + a[index] : v - a[index];
-            //printf("index: %u v: ", index);
-            if (v_delta_to_half > lut_sigma && !ramp_started && index < SAMPLERATE / 2 - 1) {
-                index++;
-                //            } else if (!ramp_started && (v1 - 2 * v) > 2) {
-                //                // pass through
-                //                printf("Compensation\n");
-            } else if (!ramp_started) {
-                // höchster Wert muss zur korrekten Berechnung 1 Mal wiederholt werden
-                ramp_started = true;
-            } else if (ramp_started && index > 0) {
-                index--;
-            }
-        }
-    } else {
-        // emergencyOverride! Potentiell schädlich für Mechanik. Nur im Notfall einsetzen!
-        index = 0;
-        v = v1;
-    }
-    return v;
+// Pins einstellen um vorwärts zu fahren
+// TODO: Richtung einstellen
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, HIGH);
+	digitalWrite(in3, LOW);
+	digitalWrite(in4, HIGH);
+
+// Geschwindigkeiten für die Motoren einstellen
+	nextVelocityPwmLeft = maxVelocityPwm;
+	nextVelocityPwmRight = maxVelocityPwm;
+
+}
+void Motor::driveStraight(unsigned char nextVelocityPwm) {
+
+// Pins einstellen um vorwärts zu fahren
+// TODO: Richtung einstellen
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, HIGH);
+	digitalWrite(in3, LOW);
+	digitalWrite(in4, HIGH);
+
+// Geschwindigkeiten für die Motoren einstellen
+	nextVelocityPwmLeft = nextVelocityPwm;
+	nextVelocityPwmRight = nextVelocityPwm;
+
 }
 
-void Motor::setPwM(unsigned char newPWM, bool dir) {
-    //analogOut(pin, newPWM);
-    //digitalOut(dirpin, dir);
-    printf("setPWM: %u, %u, %i, %u", newPWM, pin, dir, dirpin);
+void Motor::turnLeft() {
+
+// Pins einstellen um vorwärts zu fahren
+// TODO: Richtung einstellen
+	digitalWrite(in1, HIGH);
+	digitalWrite(in2, LOW);
+	digitalWrite(in3, LOW);
+	digitalWrite(in4, HIGH);
+
+// Geschwindigkeiten für die Motoren einstellen
+	nextVelocityPwmLeft = maxVelocityPwm;
+	nextVelocityPwmRight = maxVelocityPwm;
+
 }
 
-void Motor::dumpData() {
-    /* Debug-Methode.
-     * Gibt Daten und Ergebnisse von Berechnungen aus
-     */
+void Motor::turnLeft(unsigned char nextVelocityPwm) {
 
-    int a_sum = 0;
-    // Debug
+// Pins einstellen um vorwärts zu fahren
+// TODO: Richtung einstellen
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, HIGH);
+	digitalWrite(in3, LOW);
+	digitalWrite(in4, HIGH);
 
-    // Constructor:
-    printf("CONSTRUCTOR:\n");
-    printf("pin number: %u\n", pin);
-    printf("acceleration lookup table:\n");
-    for (int i = 0; i < sizeof (a) / sizeof (char); i++) {
-        printf("%3i: %i\n", i, a[i]);
-    }
-    printf("/acceleration lookup table\n");
-    for (int i = 0; i < sizeof (a) / sizeof (char); i++) {
-        a_sum += a[i];
-    }
-    printf("sum of a: %i\n", a_sum);
-    printf("/CONSTRUCTOR\n\n");
+// Geschwindigkeiten für die Motoren einstellen
+	nextVelocityPwmLeft = nextVelocityPwm;
+	nextVelocityPwmRight = nextVelocityPwm;
 
-    // calcMovement:
-    printf("CALCMOVEMENT:\n");
-    printf("v0:\t\t%3u\nv1:\t\t%3u\nv_delta:\t%3u\nv1greaterv0:\t%3i\n", v0, v1, v_delta, v1greaterv0);
-    printf("/CALCMOVEMENT\n\n");
-
-    // calcPWM:
-    printf("CALCPWM:\n");
-    for (unsigned char i = 0; i < 100; i++) {
-        printf("v %3u:\t%3u\n", i, this->calcPWM(false));
-    };
-    printf("/CALCPWM\n\n");
 }
 
+void Motor::turnRight() {
 
-// Methode zur Ansteuerung der Motoren
-void Motor::sendMotorPower(float leftWheelPower, float rightWheelPower) {
-	// TODO: Motoransteuerung realisieren.
+// Pins einstellen um vorwärts zu fahren
+// TODO: Richtung einstellen
+	digitalWrite(in1, HIGH);
+	digitalWrite(in2, LOW);
+	digitalWrite(in3, LOW);
+	digitalWrite(in4, HIGH);
+
+// Geschwindigkeiten für die Motoren einstellen
+	nextVelocityPwmLeft = maxVelocityPwm;
+	nextVelocityPwmRight = maxVelocityPwm;
+
+}
+void Motor::turnRight(unsigned char nextVelocityPwm) {
+
+// Pins einstellen um vorwärts zu fahren
+// TODO: Richtung einstellen
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, HIGH);
+	digitalWrite(in3, LOW);
+	digitalWrite(in4, HIGH);
+
+// Geschwindigkeiten für die Motoren einstellen
+	nextVelocityPwmLeft = nextVelocityPwm;
+	nextVelocityPwmRight = nextVelocityPwm;
+
+}
+
+void Motor::stop() {
+
+// Geschwindigkeiten für die Motoren einstellen
+	nextVelocityPwmLeft = 0;
+	nextVelocityPwmRight = 0;
+
+}
+void Motor::stopInstant() {
+
+}
+
+void Motor::testAnfahren() {
+
+	static int time = millis();
+
+	if(millis() >= time + 50){
+
+		time = millis();
+
+		Serial.print("links: ");
+		Serial.print(currentVelocityPwmLeft);
+		Serial.print(" ");
+		Serial.print("rechts: ");
+		Serial.print(currentVelocityPwmRight);
+		Serial.println(" ");
+
+	}
+
 }
--- Motor.h
+++ Motor.h
@@ -1,50 +1,69 @@
-
 /*
- * File:   motorsteuerung.h
- * Author: ugen
- *
- * Created on November 7, 2017, 10:16 AM
+ * 		Ansteuerung der Motoren
+ * 		Version:
+ * 		Datum:
  */
+
 
 #ifndef MOTOR_H
 #define	MOTOR_H
 
-#include <stdio.h>
-#include "Config.h"
-
 class Motor {
 private:
-    unsigned char samplerate; // Samplerate des Hauptprogramms.
-    unsigned char formfactor; // Bestimmt die Form der Beschleunigungsrampe
 
-    unsigned char v0; // alte Geschwindigkeit (PWM))
-    unsigned char v1; // geplante Geschwindigkeit (PWM))
-    unsigned char v_delta; // Geschwindigkeitsdifferenz (PWM))
-    unsigned char v_delta_half; // halbe Geschwindigkeitsdifferenz (PWM)
-    unsigned char v; // aktuelle Geschwindigkeit (PWM)
+	// Neue Version
 
-    unsigned char a[SAMPLERATE / 2]; // halbe Beschleunigungsrampe
-    unsigned char a_max; // Maximale Beschleunigung in Bit/Sample
-    unsigned char index; // Für PWM-Berechnung
+	// Pins der H Brücke
+	// Left Wheel
+	unsigned char pwmA = 10;
+	unsigned char in1 = 8;
+	unsigned char in2 = 9;
+	// Right Wheel
+	unsigned char in3 = 12;
+	unsigned char in4 = 13;
+	unsigned char pwmB = 11;
 
-    unsigned char pin;  // Analog-Out-Pin
-    unsigned char dirpin; // Digital-Out-Pin
+	// Geschwindigkeiten der Räder
+    unsigned char maxVelocityPwm = 150;			// Wert zwischen 0 und 255, maximale Geschwindigkeit
+    unsigned char nextVelocityPwmLeft = 0;		//
+    float currentVelocityPwmLeft = 0;			//
+    unsigned nextVelocityPwmRight = 0;			//
+	float currentVelocityPwmRight = 0;			//
 
-    bool v1greaterv0;   // Beschleunigung?
-    bool ramp_started;    // pos. Beschleunigungsrampe gefahren
-    bool ramp_symmetry; // Rampe muss symmetrisch sein
-    bool compensation;  // Kompensation, um Ausgabefehler gering zu halten
+	// Anfahr- und Anhaltekurvenparameter
+	float startUpTime = 1000;
+	float stopTime = 500;
+	int stepNumber = 20;
+	float startStepTime = startUpTime / stepNumber;
+	float startStepPwm = (float)maxVelocityPwm /stepNumber;
+	float stopStepTime = stopTime / stepNumber;
+	float stoptStepPwm = (float)maxVelocityPwm /stepNumber;
 
 public:
+
     Motor();
-    Motor(unsigned char pin, unsigned char dirpin);
     ~Motor(); // leerer Destruktor
 
-    void calcMovement(unsigned char v1); // Vorberechnungen für Beschleunigung auf geplante Geschwindigkeit
-    unsigned char calcPWM(bool emergencyOverride); // Berechnet anhand Beschleunigungstabelle Geschwindigkeit
-    void setPwM(unsigned char newPWM, bool dir); // Gibt PWM und Drehrichtung an Pins aus
-    void dumpData(); // Data Dump für Debugging
-    void sendMotorPower(float leftWheelPower, float rightWheelPower);
+
+    void updateVelocity();
+
+    bool isStoped();
+
+    void driveStraight();
+    void driveStraight(unsigned char nextVelocityPwm);
+    void turnLeft();
+    void turnLeft(unsigned char nextVelocityPwm);
+    void turnRight();
+    void turnRight(unsigned char nextVelocityPwm);
+
+    //void brake();
+    void stop();
+    void stopInstant();
+
+    // Testen
+    void testAnfahren();
+
+
 };
 
 #endif	/* MOTOR_H */
